11d4708d567696f564163af07d1d3971928b450b
Nicolas Modesto <modestonicolas02@gmail.com>
2025-10-14 20:53:28 -0300

    Novos perfis

--- util/perfis.py
+++ util/perfis.py
@@ -17,7 +17,8 @@ class Perfil(str, Enum):
 
     # PERFIS DO SEU SISTEMA #####################################
     ADMIN = "Administrador"
-    CLIENTE = "Cliente"
+    COMPRADOR = "Comprador"
+    VENDEDOR = "Vendedor"
     # FIM DOS PERFIS ############################################
 
     def __str__(self) -> str:
